d5348fb101ad3b5889adf8551d706107937ba101
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-21 13:08:01 +0200

    [Add] Server component

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
 
 const app = express()
 const port = process.env.PORT || 3000
 
+import Component from './src/server/gallery-app/index.jsx'
+const App = renderToString(<Component />)
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send(App)
 })
 
 app.listen(port, () => {
---
+++ src/server/gallery-app/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return(
+      <html lang="es_ES">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <title>Image Gallery</title>
+          </head>
+          <body>
+            <h1>Hello World</h1>
+          </body>
+      </html>
+    )
+  }
+}
+
+export default App
