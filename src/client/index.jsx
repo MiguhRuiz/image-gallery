7223bf81efc3acf2c1e22103e0592b788477a54d
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-23 18:33:41 +0200

    [Update] Make SPA works

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
+import Base from './base-component/index.jsx'
 import ImageContainer from './image-container/index.jsx'
 import ImageForm from './upload-form/index.jsx'
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={ImageContainer}>
+    <Route path="/" component={Base}>
+      <IndexRoute component={ImageContainer} />
       <Route path="/new" component={ImageForm} />
     </Route>
   </Router>
