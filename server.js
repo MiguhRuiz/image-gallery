56bef3d108a0046907319ee9a06946dd5bf04f38
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-23 17:09:00 +0200

    [Add] PouchDB express middleware in order to make deploys

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ import express from 'express'
 import BodyParser from 'body-parser'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import PouchDB from 'pouchdb'
+import dbUtility from 'express-pouchdb'
+
 import api from './src/server/routes/api'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const db = dbUtility(PouchDB)
 
 import Component from './src/server/components/gallery-app/index.jsx'
 const App = renderToString(<Component />)
 
 app.use(BodyParser.json())
 app.use('/public', express.static('public'))
+app.use('/db', db)
 app.use('/api', api)
 
 app.get('/', (req, res) => {
